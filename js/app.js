3066235517e32be58dcfbfe9a779fed28554b1c7
Sarah <sarahpenneyuk@gmail.com>
2022-10-12 15:54:58 +0100

    .

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
-// function pictureChange() {
-//     document.getElementById('theImage').src="images/DM_DT.png";
-// }
+
+
 
 var image_tracker = 'orange';
 
@@ -19,11 +18,6 @@ var image_tracker = 'orange';
  var elem = document.getElementById("theButton");
  if (elem.value=="switch to: DARKMODE") elem.value = "switch to: LIGHTMODE";
  else elem.value = "switch to: DARKMODE";
-
-
-
-
-
  }
 
 
@@ -49,42 +43,3 @@ var image_tracker = 'orange';
 
  }
 
-
-
-
-// (function() {
-//   function scrollHorizontally(e) {
-//     e = window.event || e;
-//     var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
-//     document.documentElement.scrollLeft -= (delta*40); // Multiplied by 40
-//     document.body.scrollLeft -= (delta*40); // Multiplied by 40
-//     e.preventDefault();
-//   }
-//   if (window.addEventListener) {
-//     // IE9, Chrome, Safari, Opera
-//     window.addEventListener('mousewheel', scrollHorizontally, false);
-//     // Firefox
-//     window.addEventListener('DOMMouseScroll', scrollHorizontally, false);
-//   } else {
-//     // IE 6/7/8
-//     window.attachEvent('onmousewheel', scrollHorizontally);
-//   }
-// })();
-
-// $(function() {
-//   console.log('werk');
-//   $('ul.nav a').bind('click',function(event){
-//     var $anchor = $(this);
-//                     /*
-//                     if you want to use one of the easing effects:
-//                     $('html, body').stop().animate({
-//                         scrollLeft: $($anchor.attr('href')).offset().left
-//                     }, 1500,'easeInOutExpo');
-//                      */
-//     $('html, body').stop().animate({
-//       scrollLeft: $($anchor.attr('href')).offset().left
-//     }, 1000);
-//     console.log('werk2');
-//     event.preventDefault();
-//   });
-// });
